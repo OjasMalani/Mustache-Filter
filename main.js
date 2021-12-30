43aa8362cb9c82f24d5bb83f7088165d0b16ea70
OjasMalani <79085373+OjasMalani@users.noreply.github.com>
2021-12-30 14:53:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
-    //leave this empty
+    clown_nose= loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
     }
     
     function setup() {
@@ -9,11 +12,12 @@ function preload() {
         video.size(350,350);
         video.hide();
         poseNet= ml5.poseNet(video,modelLoaded);
-        poseNet.on('Pose', gotPoses)
+        poseNet.on('pose', gotPoses)
     }
     
     function draw() {
-        image(video,0,0,350,350)
+        image(video,0,0,350,350);
+        image(clown_nose,noseX,noseY,80,35);
     }
     
     function take_snapshot() {
@@ -27,7 +31,7 @@ function preload() {
     function gotPoses(results) {
         if(results.length>0) {
             console.log(results);
-            console.log("Nose x=" + results[0].pose.nose.x);
-            console.log("Nose y=" + results[0].pose.nose.y)
+            noseX= results[0].pose.nose.x-40;
+            noseY= results[0].pose.nose.y+10;
         }
     } 
